8d3747cf99de47521d029c0c891c03f790bb7b1c
zhanght <983033995@qq.com>
2020-05-27 16:19:59 +0800

    创建项目

---
+++ src/views/About.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="about">
+    <button @click="add">点我</button>
+
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  name: "about",
+  setup(props, context) {
+    let text = "asasdfd";
+    console.log("props", props);
+    console.log("context", context);
+    onMounted(() => {
+      console.log("context___", context.text);
+    });
+    const add = function() {
+      text = "add";
+      console.log("text");
+    };
+    return {
+      text,
+      add
+    };
+  },
+  onMounted() {
+    console.log("________onMounted");
+  },
+  mounted() {
+    console.log("________mounted", this);
+  }
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,41 @@
+let path = require("path")
+let env = process.env
+console.log("env", env)
+module.exports = {
+  publicPath: "./",
+  chainWebpack: (config) => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"]
+    types.forEach((type) => {
+      addStyleResource(config.module.rule("less").oneOf(type))
+    })
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        "resolve url": true,
+        // 使用stylus文件
+        //   import: ['./src/assets/styles/xxx.styl']
+      },
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  pluginOptions: {
+    //   "cube-ui": {
+    //       postCompile: true,
+    //       theme: true
+    //   }
+  },
+  devServer: {},
+}
+
+// 添加全局公共样式
+function addStyleResource(rule) {
+  rule
+    .use("style-resource")
+    .loader("style-resource-loader")
+    .options({
+      patterns: [path.resolve(__dirname, "src/assets/styles/global.less")],
+    })
+}
